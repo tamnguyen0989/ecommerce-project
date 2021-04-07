d6146d76d0ee18957755d586ba5adfb84d8bbb00
tamnguyen <tamnguyen0989@gmail.com>
2021-04-08 06:13:01 +0700

    implement directory.component.js

---
+++ src/components/directory/directory.component.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class Directory extends Component {
+    constructor(){
+        super()
+        
+        this.state ={
+            sections : [
+                
+            ]
+        }
+    }
+
+    render() {
+        return (
+            <div className='directory-menu'>
+                
+            </div>
+        );
+    }
+}
+
+export default Directory;
