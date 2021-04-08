479ab56f3a1900b9155174460e5693a75dfaf0d9
TITANCORP\tam.nguyenh <tam.nguyenh@titancorpvn.com>
2021-04-08 11:16:33 +0700

    test

--- src/components/menu-item/menu-item.component.js
+++ src/components/menu-item/menu-item.component.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import './menu-item.styles.scss';
 
-const MenuItem = ({ title }) => {
+const MenuItem = ({ title, imageUrl, size }) => {
     return (
-        <div className='menu-item'>
+        <div className={`${size} menu-item`}>
+            <div
+                className='background-image'
+                style={{
+                    backgroundImage: `url(${imageUrl})`
+                }}
+            />
             <div className='content'>
-                <div className='title'>{title}</div>
+                <h1 className='title'>{title.toUpperCase()}</h1>
                 <span className='subtitle'>SHOP NOW</span>
             </div>
         </div>
